848a3ed6861fa6f111cf234e923d97e6497935c3
Druva2804 <druva.bhagavatula@gmail.com>
2025-02-09 23:31:22 +0530

    Page Rendering

---
+++ index.js
@@ -0,0 +1,51 @@
+const express=require("express");
+const app=express();
+const port=8080;
+const path=require("path");
+
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.listen(port,()=>{
+    console.log(`Server is running at ${port}`);
+});
+
+app.get("/",(req,res)=>{
+    res.end("hello everyone");
+})
+
+let posts=[
+    {
+        username:"Druv",
+        content:"pending"
+    },
+    {
+        username:"Vivekan",
+        content:"blogging"
+    },
+    {
+        username:"Shraan",
+        content:"Acting"
+    },
+];
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
+});
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res)=>{
+    let {username,content}=req.body;
+    posts.push({username,content});
+    res.redirect("/posts");
+    // res.send("post request received");
+    // console.log(req.body);
+    // res.send("Data is received..");
+});
